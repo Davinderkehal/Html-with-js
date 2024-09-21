3cb70e952d0a61942ed7956b3b2c731c01b8f48d
Davinderkehal <n01718686@humber.ca>
2024-09-21 00:07:25 -0400

    add both files

---
+++ script.js
@@ -0,0 +1,102 @@
+console.log("Hello World!")
+
+var studentName = "Alex";
+console.log(studentName)
+studentName = "jordan";
+console.log(studentName);
+var studentName = "Taylor"
+console.log(studentName);
+
+
+
+let myName = "Davinder Kehal";
+
+let myAge =54
+
+console.log(myAge);
+
+//console.log(`my name is ${myName} my age is ${myAge}`);
+
+
+let win = true;
+let lost = false;
+console.log(win);
+console.log(lost );
+
+// Number
+let length = 100; 
+
+// BigInt 
+let bigNumber =1000199254741001n;
+console.log(length);
+console.log(bigNumber);
+
+//All three variables represent strings
+let firstName = "Davinder";
+let middleName = 'Singh';
+let lastName = `kehal`;
+
+let cars = ['TOYOTA', 'Lexus', 'BMW'];
+console.log(cars[0]); 
+cars.push('Honda'); 
+console.log(cars); 
+cars.pop();
+console.log(cars);
+
+const person = new Object();
+person .age = 54;
+person .name = "Davinder";
+
+console.log(person.name);
+
+//basic Arthmatics
+
+console.log(2 * 2); 
+console.log(32 / 4); 
+console.log(47 % 5); 
+
+console.log(22 + 33); 
+console.log(17 - 7);
+
+//Comparison Operators
+
+let a = 5; 
+let b = 10;
+console.log("10" == b); 
+
+// Equality
+
+console.log("10" === 3);
+
+// Strict equality
+console.log(a != b);
+// Inequality
+
+console.log(a < b); 
+// Less than
+
+
+console.log(a > b);
+ // Greater than
+
+
+console.log(a <= b);
+ // Less than or equal to
+
+
+console.log(a >= b);
+// Greater than or equal to
+
+//Logical operators
+
+let x = true;
+let y = false;
+
+// Logical AND
+console.log(x && y);
+
+// Logical OR
+console.log(x || y);
+
+// Logical NOT
+console.log(!x); 
